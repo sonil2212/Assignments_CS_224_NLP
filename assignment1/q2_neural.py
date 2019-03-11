c1900e69f694a67f11d56abc1c63ee243f14ca7d
sonil2212 <sonil.s@edgenetworks.in>
2019-03-11 12:53:01 +0530

    updated_scripts

---
+++ assignment1/q2_neural.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import numpy as np
+import random
+
+from q1_softmax import softmax
+from q2_sigmoid import sigmoid, sigmoid_grad
+from q2_gradcheck import gradcheck_naive
+
+
+def forward_backward_prop(X, labels, params, dimensions):
+    """
+    Forward and backward propagation for a two-layer sigmoidal network
+
+    Compute the forward propagation and for the cross entropy cost,
+    the backward propagation for the gradients for all parameters.
+
+    Notice the gradients computed here are different from the gradients in
+    the assignment sheet: they are w.r.t. weights, not inputs.
+
+    Arguments:
+    X -- M x Dx matrix, where each row is a training example x.
+    labels -- M x Dy matrix, where each row is a one-hot vector.
+    params -- Model parameters, these are unpacked for you.
+    dimensions -- A tuple of input dimension, number of hidden units
+                  and output dimension
+    """
+
+    ### Unpack network parameters (do not modify)
+    ofs = 0
+    Dx, H, Dy = (dimensions[0], dimensions[1], dimensions[2])
+    W1 = np.reshape(params[ofs:ofs+ Dx * H], (Dx, H))
+    ofs += Dx * H
+    b1 = np.reshape(params[ofs:ofs + H], (1, H))
+    ofs += H
+    W2 = np.reshape(params[ofs:ofs + H * Dy], (H, Dy))
+    ofs += H * Dy
+    b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
+
+    # Note: compute cost based on `sum` not `mean`.
+    ### YOUR CODE HERE: forward propagation
+    #raise NotImplementedError
+    
+    h=sigmoid(np.dot(X,W1)+b1)
+    z=np.dot(h,W2)+b2
+    o=softmax(z)
+
+    cost=-np.sum(labels*np.log(o))
+    #print cost
+    ## END YOUR CODE
+
+    ## YOUR CODE HERE: backward propagation
+    delta=o-labels
+    gradW2=np.dot(h.T,delta)
+    
+    gradb2=np.sum(delta,axis=0)
+
+    temp=np.dot(delta,W2.T)
+    dh=sigmoid_grad(h)*temp
+    gradW1=np.dot(X.T,dh)
+
+    gradb1=np.sum(dh,axis=0)
+    
+    ## END YOUR CODE
+
+    ## Stack gradients (do not modify)
+    grad = np.concatenate((gradW1.flatten(), gradb1.flatten(),
+            gradW2.flatten(), gradb2.flatten()))
+    
+    return cost, grad
+
+
+def sanity_check():
+    """
+    Set up fake data and parameters for the neural network, and test using
+    gradcheck.
+    """
+    print "Running sanity check..."
+
+    N = 20
+    dimensions = [10, 5, 10]
+    data = np.random.randn(N, dimensions[0])   # each row will be a datum
+    labels = np.zeros((N, dimensions[2]))
+    for i in xrange(N):
+        labels[i, np.random.randint(0,dimensions[2]-1)] = 1
+
+    params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (dimensions[1] + 1) * dimensions[2], )
+    #print params[0],params[1]
+    gradcheck_naive(lambda params:forward_backward_prop(data,labels,params,dimensions),params)
+
+
+def your_sanity_checks():
+    """
+    Use this space add any additional sanity checks by running:
+        python q2_neural.py
+    This function will not be called by the autograder, nor will
+    your additional tests be graded.
+    """
+    print "Running your sanity checks..."
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+
+
+if __name__ == "__main__":
+    sanity_check()
+    #your_sanity_checks()
